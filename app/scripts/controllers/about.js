2fba44acb30deb78b21bec24bda8afc394ca774f
Joseph McConnell <jjmdevelops@gmail.com>
2014-09-29 00:41:42 -0400

    Yo Scaffolding
    Initial creation using the Yo Generator

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sentenceUnscrambleApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sentenceUnscrambleApp
+ */
+angular.module('sentenceUnscrambleApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sentenceUnscrambleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the sentenceUnscrambleApp
+ */
+angular.module('sentenceUnscrambleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
